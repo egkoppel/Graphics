2c660005632e65b5cbc70b89b4e20601bbd7d5b0
bluewhale64 <whalenoahmj@hotmail.com>
2021-10-26 01:48:42 +0100

    Added two shaders from an old Voronoi project: https://www.github.com/bluewhale64/Voronoi

---
+++ shaders/euclideanfragmentnoseeds.glsl
@@ -0,0 +1,24 @@
+#version 330 core
+layout(location = 0) out vec4 color;
+layout(origin_upper_left) in vec4 gl_FragCoord;
+uniform float u_height;
+uniform float u_width;
+uniform int u_seedx[50];
+uniform int u_seedy[50];
+uniform float u_seedc[150];
+void main(){
+	float x = gl_FragCoord.x;
+	float y = gl_FragCoord.y;
+	int n = 0;
+	float maxdistance = sqrt((u_height * u_height) + (u_width * u_width));
+	float dist = maxdistance;
+	float distancecurrent;
+	for(int i = 0; i < 50; i++){
+		distancecurrent = sqrt( ( (x - float(u_seedx[i])) * (x - float(u_seedx[i])) ) + ( (y - float(u_seedy[i])) * (y - float(u_seedy[i])) ) );
+		if(distancecurrent < dist){
+			dist = distancecurrent;
+			n = i;
+		}
+	}
+	color = vec4(u_seedc[(n*3)+0], u_seedc[(n*3)+1], u_seedc[(n*3)+2], 1.0f);
+}
