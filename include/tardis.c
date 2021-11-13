8fc7d330b903a997c1d1e6b5686f61d1f4df0cd2
bluewhale64 <whalenoahmj@hotmail.com>
2021-11-13 15:29:17 +0000

    Fixed coordinates so all triangles are anticlockwise-indexed

--- include/tardis.c
+++ include/tardis.c
@@ -27,16 +27,16 @@
     unsigned int tardisindices[] = {
         0, 1, 2, //base
         2, 3, 0,
-        0, 1, 4, //front
+        1, 0, 4, //front
         4, 5, 1,
-        2, 3, 6, //back
+        3, 2, 6, //back
         6, 7, 3,
         0, 3, 4, //left
-        4, 7, 3,
-        1, 2, 5, //right
+        7, 4, 3,
+        2, 1, 5, //right
         5, 6, 2,
-        4, 5, 8, //top front
+        5, 4, 8, //top front
         4, 7, 8, //top left
-        6, 7, 8, //top back
-        5, 6, 8  //top right
+        7, 6, 8, //top back
+        6, 5, 8  //top right
     };
